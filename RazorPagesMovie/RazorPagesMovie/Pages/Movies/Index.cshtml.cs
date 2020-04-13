f3dca9814dcbd071e37c361365bb6bf772d50935
jjyoon7 <jay.yoon7@gmail.com>
2020-04-13 16:39:49 +0200

    Language-Integrated Query (LINQ) enables you to write queries against strongly typed collections of objects by using language keywords and familiar operators.

--- RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -33,6 +33,14 @@ namespace RazorPagesMovie.Pages.Movies
 
         public async Task OnGetAsync()
         {
+            // using System.Linq;
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
             Movie = await _context.Movie.ToListAsync();
         }
     }
